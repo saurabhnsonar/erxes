c692370ab9fbbacfb47022c1187fd90c34735cfc
batamar <batamar.b@nmtec.co>
2017-10-12 19:11:50 +0800

    Add conversations list to customer detail

--- imports/react-ui/customers/components/detail/CustomerDetails.js
+++ imports/react-ui/customers/components/detail/CustomerDetails.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Wrapper } from '/imports/react-ui/layout/components';
 import { List as InternalNotes } from '/imports/react-ui/internalNotes/containers';
+import { ConversationsList, EmptyState } from '/imports/react-ui/common';
 import RightSidebar from './sidebar/RightSidebar';
 import LeftSidebar from './sidebar/LeftSidebar';
 
@@ -14,8 +15,50 @@ const propTypes = {
 };
 
 class CustomerDetails extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { currentTab: 'internalNotes' };
+
+    this.onTabClick = this.onTabClick.bind(this);
+  }
+
+  onTabClick(currentTab) {
+    this.setState({ currentTab });
+  }
+
+  renderTabContent() {
+    const { currentTab } = this.state;
+    const { customer } = this.props;
+
+    if (currentTab === 'internalNotes') {
+      return <InternalNotes contentType="customer" contentTypeId={customer._id} />;
+    }
+
+    if (currentTab === 'conversations') {
+      return this.renderConversations();
+    }
+  }
+
+  renderConversations() {
+    const { customer } = this.props;
+    const conversations = customer.conversations;
+
+    return (
+      <div style={{ position: 'relative' }}>
+        {conversations.length
+          ? <ConversationsList conversations={conversations} user={Meteor.user()} />
+          : <EmptyState
+              text="There aren’t any conversations at the moment."
+              icon={<i className="ion-email" />}
+            />}
+      </div>
+    );
+  }
+
   render() {
     const { customer } = this.props;
+    const { currentTab } = this.state;
 
     const breadcrumb = [
       { title: 'Customers', link: FlowRouter.path('customers/list') },
@@ -25,21 +68,21 @@ class CustomerDetails extends React.Component {
     const content = (
       <div className="customer-detail-content">
         <ul className="header">
-          <li className="active">
-            <a>
+          <li className={currentTab === 'internalNotes' ? 'active' : ''}>
+            <a onClick={() => this.onTabClick('internalNotes')}>
               <i className="ion-email" />
               New note
             </a>
           </li>
-          <li>
-            <a>
+          <li className={currentTab === 'conversations' ? 'active' : ''}>
+            <a onClick={() => this.onTabClick('conversations')}>
               <i className="ion-paper-airplane" />
               Conversations
             </a>
           </li>
         </ul>
 
-        <InternalNotes contentType="customer" contentTypeId={customer._id} />
+        {this.renderTabContent()}
       </div>
     );
 
